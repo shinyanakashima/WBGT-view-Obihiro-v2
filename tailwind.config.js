4165cfa0ca8879118365d7c8c30fa86d648b4ab6
shinya <shinya.gp@gmail.com>
2025-06-16 11:25:33 +0900

    chore: Add font config.#9

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [
+                    'IPAPGothic',       // IPA Pゴシック（等幅っぽい）
+                    'IPAGothic',        // IPAゴシック（より標準的）
+                    'Noto Sans JP',     // Google 日本語フォント
+                    'Hiragino Sans',    // macOS 系
+                    'Meiryo',           // Windows
+                    'Yu Gothic',
+                    'sans-serif',
+                ],
+            },
+        },
+    },
+};
